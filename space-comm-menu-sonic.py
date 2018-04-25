ff8a3d6de7a4877257da87730b612d58db7267d3
Ryan Stong <rvanstong@gmail.com>
2018-04-24 20:38:12 -0700

    adds in scanning animation for sonic

--- space-comm-menu-sonic.py
+++ space-comm-menu-sonic.py
@@ -70,9 +70,72 @@ sonic_menu3 = Image("00000:"
                     "00000:")
 sonic_menu = [sonic_menu1, sonic_menu2, sonic_menu3]
 
-flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+scanning1 = Image("90000:"
+                    "90000:"
+                    "90000:"
+                    "90000:"
+                    "90000:")
+scanning2 = Image("79000:"
+                    "79000:"
+                    "79000:"
+                    "79000:"
+                    "79000:")
+scanning3 = Image("57900:"
+                    "57900:"
+                    "57900:"
+                    "57900:"
+                    "57900:")
+scanning4 = Image("45790:"
+                    "05790:"
+                    "05790:"
+                    "45790:"
+                    "05790:")
+scanning5 = Image("04579:"
+                    "00579:"
+                    "04579:"
+                    "00579:"
+                    "04579:")
+scanning6 = Image("00479:"
+                    "00479:"
+                    "00079:"
+                    "00479:"
+                    "00479:")
+scanning7 = Image("00000:"
+                    "00000:"
+                    "00000:"
+                    "00000:"
+                    "00000:")
+scanning8 = Image("00097:"
+                    "00097:"
+                    "00097:"
+                    "00097:"
+                    "00097:")
+scanning9 = Image("00975:"
+                    "00975:"
+                    "00975:"
+                    "00975:"
+                    "00975:")
+scanning10 = Image("09754:"
+                    "09750:"
+                    "09754:"
+                    "09750:"
+                    "09754:")
+scanning11 = Image("97500:"
+                    "97540:"
+                    "97540:"
+                    "97540:"
+                    "97500:")
+scanning12 = Image("95400:"
+                    "95400:"
+                    "95000:"
+                    "95400:"
+                    "95400:")
+
+scanning_cycle1 = [scanning1, scanning2, scanning3, scanning4, scanning5, scanning6]
+scanning_cycle2 = [scanning7, scanning8, scanning9, scanning10, scanning11, scanning12]
 
 flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+
 menu_items = [radio_menu, temp_menu, sonic_menu]
 menu_counter = 0
 
@@ -108,10 +171,14 @@ while True:
         y = accelerometer.get_y()
         if y < 0:
             y = y * -1
-        music.pitch(x, 10)
+        music.pitch(x, 20)
         music.pitch(200, 15)
-        music.pitch(y, 10)
+        display.show(scanning_cycle1, delay=100)
+        music.pitch(y, 20)
         music.pitch(500, 15)
+        display.show(scanning_cycle2, delay=100)
+        display.clear()
+
 
     incoming = radio.receive()
     if incoming == 'send':
