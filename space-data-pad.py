6d24bfc3aeb9c921dc2c9f91a863dc2aa69002a5
Ryan Stong <rvanstong@gmail.com>
2018-04-28 08:46:58 -0700

    still working on datapad, will introduce button matrix code once I build the hardware

--- space-data-pad.py
+++ space-data-pad.py
@@ -1,121 +1,74 @@
 from microbit import *
 
-# pin connections
-rs = pin0
-enable = pin1
-datapins = [pin8, pin12, pin2, pin13]
+scanning1 = Image("90000:"
+                    "00000:"
+                    "90000:"
+                    "00000:"
+                    "90000:")
+scanning2 = Image("79000:"
+                    "00000:"
+                    "79000:"
+                    "00000:"
+                    "79000:")
+scanning3 = Image("57900:"
+                    "00000:"
+                    "57900:"
+                    "00000:"
+                    "57900:")
+scanning4 = Image("45790:"
+                    "00000:"
+                    "05790:"
+                    "00000:"
+                    "05790:")
+scanning5 = Image("04579:"
+                    "00000:"
+                    "04579:"
+                    "00000:"
+                    "04579:")
+scanning6 = Image("00479:"
+                    "00009:"
+                    "00079:"
+                    "00009:"
+                    "00479:")
+scanning7 = Image("00000:"
+                    "00000:"
+                    "00000:"
+                    "00000:"
+                    "00000:")
+scanning8 = Image("00000:"
+                    "00097:"
+                    "00000:"
+                    "00097:"
+                    "00000:")
+scanning9 = Image("00000:"
+                    "00975:"
+                    "00000:"
+                    "00975:"
+                    "00000:")
+scanning10 = Image("00000:"
+                    "09750:"
+                    "00000:"
+                    "09750:"
+                    "00000:")
+scanning11 = Image("00000:"
+                    "97540:"
+                    "00000:"
+                    "97540:"
+                    "00000:")
+scanning12 = Image("90000:"
+                    "95400:"
+                    "90000:"
+                    "95400:"
+                    "90000:")
 
-# commands
-LCD_CLEARDISPLAY = 0x01
-LCD_RETURNHOME = 0x02
-LCD_ENTRYMODESET = 0x04
-LCD_DISPLAYCONTROL = 0x08
-LCD_CURSORSHIFT = 0x10
-LCD_FUNCTIONSET = 0x20
-LCD_SETCGRAMADDR = 0x40
-LCD_SETDDRAMADDR = 0x80
+scanning_cycle1 = [scanning1, scanning2, scanning3, scanning4, scanning5, scanning6]
+scanning_cycle2 = [scanning7, scanning8, scanning9, scanning10, scanning11, scanning12]
 
-# flags for display entry mode
-LCD_ENTRYRIGHT = 0x00
-LCD_ENTRYLEFT = 0x02
-LCD_ENTRYSHIFTINCREMENT = 0x01
-LCD_ENTRYSHIFTDECREMENT = 0x00
+flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
 
-# flags for display on/off control
-LCD_DISPLAYON = 0x04
-LCD_DISPLAYOFF = 0x00
-LCD_CURSORON = 0x02
-LCD_CURSOROFF = 0x00
-LCD_BLINKON = 0x01
-LCD_BLINKOFF = 0x00
-
-# flags for display/cursor shift
-LCD_DISPLAYMOVE = 0x08
-LCD_CURSORMOVE = 0x00
-LCD_MOVERIGHT = 0x04
-LCD_MOVELEFT = 0x00
-
-# flags for function set
-LCD_8BITMODE = 0x10
-LCD_4BITMODE = 0x00
-LCD_2LINE = 0x08
-LCD_1LINE = 0x00
-LCD_5x10DOTS = 0x04
-LCD_5x8DOTS = 0x00
-
-
-def InitDisplay():
-    display.set_pixel(0,2,9)
-    # at least 50ms after power on
-    sleep(50)
-    # send rs, enable low - rw is tied to GND
-    rs.write_digital(0)
-    enable.write_digital(0)
-    write4bits(0x03)
-    sleep(5)
-    write4bits(0x03)
-    sleep(5)
-    write4bits(0x03)
-    sleep(2)
-    write4bits(0x02)
-    send(LCD_FUNCTIONSET | 0x08, 0)
-    sleep(5)
-    send(LCD_FUNCTIONSET | 0x08, 0)
-    sleep(2)
-    send(LCD_FUNCTIONSET | 0x08, 0)
-    sleep(2)
-    send(LCD_FUNCTIONSET | 0x08, 0)
-    sleep(2)
-    send(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF,0)
-    clear()
-    send(LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT,0)
-    display.set_pixel(1,2,9)
-
-
-# high level commands
-def clear():
-    send(LCD_CLEARDISPLAY,0)
-    sleep(2)
-
-def home():
-    send(LCD_RETURNHOME,0)
-    sleep(2)
-
-def setCursor(col, row):
-    orpart = col
-    if row>0:
-        orpart = orpart + 0x40
-    send(LCD_SETDDRAMADDR | orpart, 0)
-
-def showText(t):
-    for c in t:
-        send(ord(c), 1)
-
-# mid and low level commands
-def send(value, mode):
-    rs.write_digital(mode)
-    write4bits(value>>4)
-    write4bits(value)
-
-def pulseEnable():
-    enable.write_digital(0)
-    sleep(1)
-    enable.write_digital(1)
-    sleep(1)
-    enable.write_digital(0)
-    sleep(1)
-
-def write4bits(value):
-    for i in range(0,4):
-        datapins[i].write_digital((value>>i) & 0x01)
-    pulseEnable()
-
-display.set_pixel(2,2,5)
-# Test
-InitDisplay()
-showText("Hello")
-setCursor(0,1)
-showText("World")
-sleep(5000)
-clear()
-showText("How are you?")
+while True:
+    if button_a.was_pressed():
+        for i in range(0,5,1):
+            display.show(scanning_cycle1, delay=50)
+            display.show(scanning_cycle2, delay=50)
+            display.clear()
